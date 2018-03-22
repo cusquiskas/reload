398bbf15947f6a9ef1b6487b625402cae0e9415f
cusquiskas <cusquiskas@gmail.com>
2018-03-22 01:46:09 +0100

    configuración de los controladores

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+let Moduls = [];
 $(document).ready(function (){
-    console.log('ya tenemos el body cargado');
+    let Template = document.getElementsByTagName('template');
+    for (let i = 0; i < Template.length; i++) 
+        Moduls[Template[i].id] = new ModulController(Template[i], null);
+    //... inicialmente, no debería haber ningún formulario fuera de un template ...//
+    // Moduls.Forms = [];
+    // for (let i = 0; i < document.forms.length; i++) 
+    //     Moduls.Forms[document.forms[i].name] = new FormController(document.forms[i], null);
 });
